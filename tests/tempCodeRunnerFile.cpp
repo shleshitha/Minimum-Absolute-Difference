44cfbdc8d02cdaafbf953aa2312bd008c47364e2
Shlesh <mail2shleshitha@gmail.com>
2025-05-30 14:27:55 +0530

    cpp file modified

--- tests/tempCodeRunnerFile.cpp
+++ tests/tempCodeRunnerFile.cpp
@@ -1,4 +1,5 @@
-for (int num : input) cout << num << " ";
-        // cout << "\nOutput: ";
-        // for (const auto& p : output)
-        //     c
+ // cout << "Input (first 10): ";
+        // for (int i = 0; i < min(10, (int)input.size()); ++i) cout << input[i] << " ";
+        // cout << "\nExpected: "; printVectorPairs(expected);
+        // cout << "\nGot     : "; printVectorPairs(vector<vector<int>>(result.begin(), result.begin() + expected.size()));
+        // cou
--- tests/test.cpp
+++ tests/test.cpp
@@ -41,11 +41,6 @@ void runTestCase(const vector<int>& input, const vector<vector<int>>& expected, 
         cout << "✅ Test " << total << " passed.\n";
     } else {
         cout << "❌ Test " << total << " failed.\n";
-        cout << "Input (first 10): ";
-        for (int i = 0; i < min(10, (int)input.size()); ++i) cout << input[i] << " ";
-        cout << "\nExpected: "; printVectorPairs(expected);
-        cout << "\nGot     : "; printVectorPairs(vector<vector<int>>(result.begin(), result.begin() + expected.size()));
-        cout << "\n\n";
     }
 }
 
