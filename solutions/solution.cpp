ed40d9de5322e716d51b851e95663196371a06bc
Shleshitha Cheekati <153906990+shleshitha@users.noreply.github.com>
2025-05-30 10:30:23 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -8,19 +8,7 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        vector<vector<int>> res;
-        int minDiff = INT_MAX;
-        for (int i = 1; i < arr.size(); ++i) {
-            int diff = arr[i] - arr[i - 1];
-            if (diff < minDiff) {
-                minDiff = diff;
-                res.clear();
-            }
-            if (diff == minDiff) {
-                res.push_back({arr[i - 1], arr[i]});
-            }
-        }
-        return res;
+        
+        return {};
     }
 };
